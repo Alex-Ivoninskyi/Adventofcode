940a18bceb83615598cd12e51653b3534487938a
Alex-Ivoninskyi <76066635+Alex-Ivoninskyi@users.noreply.github.com>
2021-01-28 20:19:39 +0200

    Update Day1_1.pas

--- 2015/Day01/Day1_1.pas
+++ 2015/Day01/Day1_1.pas
@@ -1,14 +1,14 @@
 Program Day1_1;
 var f: text;
-    s:string;
-    i,ans:integer;
+    s: string;
+    i, ans: integer;
 begin
-  Assign(f,'input.txt');
+  Assign(f, 'input.txt');
   reset(f);
-  read(f,s);
-  for i:=1 to length(s) do
-    if s[i]='(' then ans:=ans+1 else ans:=ans-1;
-    writeln(ans);
-
-  
+  read(f, s);
+  for i:= 1 to length(s) do
+    if s[i] = '(' then 
+      ans:= ans + 1 
+    else ans:= ans - 1;
+      writeln(ans); 
 end.
